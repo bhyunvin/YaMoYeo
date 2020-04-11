dff3610dc716bc7ebc592cac532181bac4033438
백종현 <bhyunvin@naver.com>
2020-04-11 13:58:10 +0900

    인코딩문제관련 테스트
    인코딩문제관련 테스트 야모여파일, 세팅파일 업로드

--- src/com/kh/innerFrendship/YaMoYeo/YaMoYeo.java
+++ src/com/kh/innerFrendship/YaMoYeo/YaMoYeo.java
@@ -2,6 +2,6 @@ package com.kh.innerFrendship.YaMoYeo;
 
 public class YaMoYeo {
 	public static void main(String[] args) {
-		System.out.println("4조 내적친목 팀의 야!모여 프로젝트 입니다.");
+		System.out.println("4조 내적친목 팀의 야!모여 프로젝트 입니다."); 
 	}
 }
